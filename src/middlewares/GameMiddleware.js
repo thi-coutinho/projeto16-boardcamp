5dbaca16188fe0c2986c6c8921adcd35804a9d2d
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-02-10 09:48:23 -0300

    fix: validateGame return when game already in db

--- src/middlewares/GameMiddleware.js
+++ src/middlewares/GameMiddleware.js
@@ -4,7 +4,7 @@ export async function ValidateGame(req, res, next) {
     const game = req.body.name
     try {
         const checkGame = await db.query("SELECT * FROM games WHERE name = $1",[game]);
-        if (checkGame.rows[0]) return res.status(409)
+        if (checkGame.rows[0]) return res.sendStatus(409)
     } catch (error) {
         return res.status(500).send(error.message)
     }
