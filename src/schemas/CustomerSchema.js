dd18757bf763c0ac2902b40c12ff32124cac4203
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-02-10 11:43:27 -0300

    fix: customerSchema blocks birthdays after now

--- src/schemas/CustomerSchema.js
+++ src/schemas/CustomerSchema.js
@@ -5,5 +5,5 @@ export const CustomerSchema = joi.object({
     name: joi.string().not(null).required(),
     phone: joi.string().min(10).max(11).pattern(/^[0-9]+$/).required(),    
     cpf: joi.string().length(11).pattern(/^[0-9]+$/).required(),    
-    birthday: joi.date().required(),
+    birthday: joi.date().less('now').required(),
 });
